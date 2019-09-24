ceec6cd2f0215362d55b753f58b96689b79b1ba0
sacusa <sudhanshugupta96@gmail.com>
2019-09-24 17:37:53 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python
+import sys
+
+'''
+ 13   12   11   10     9    8    7        ========> Pit numbers
+    --------------------------------
+ 00 | 04 | 04 | 04 || 04 | 04 | 04 | P2
+----------------------------------------
+ P1 | 04 | 04 | 04 || 04 | 04 | 04 | 00
+    --------------------------------
+       0    1    2     3    4    5    6   ========> Pit numbers
+'''
+
+NUM_STONES = 4
+NUM_PITS = 14
+
+class state():
+    def __init__(self, board):
+        self.board = board
+        self.is_terminal = False
+        self.utility = 0
+
+        if (sum(self.board[0:6]) == 0) or (sum(self.board[7:13]) == 0):
+            self.is_terminal = True
+            self.utility = self.board[6] - self.board[13]
+
+        self.min = 0
+        self.max = 0
+        self.result_of = {i:None for i in range(NUM_PITS)}
+    
+    def result(self, action):
+        # return the precomputed next state if we have it
+        #================================================
+        if self.result_of[action]:
+            return self.result_of[action]
+
+        # we don't have the next state precomputed => compute and store it
+        #=================================================================
+        next_board = self.board[:]
+
+        start_pit = action + 1
+        end_pit = action + board[action]
+        
+        # make sure we don't accidentally fill-in the opponent's store
+        pit_to_skip = -1
+        if action < 6 and end_pit >= 13:
+            pit_to_skip = 13
+        if action > 6 and (end_pit % NUM_PITS) >= 6
+            pit_to_skip = 6
+
+        # empty the current pit and fill in others
+        next_board[action] = 0
+        for pit in range(start_pit, end_pit + 1):
+            pit %= NUM_PITS
+            if pit == pit_to_skip:
+                continue
+            next_board[pit] += 1
+        
+        # update the dictionary of resultant states
+        next_state = state(next_board)
+        self.result_of[action] = next_state
+
+        return next_state
+    
+    def is_terminal_state(self):
+        return self.is_terminal
+    
+    def is_valid_action(self, action):
+        return ((action >= 0 and action < 13) and
+                (action != 6) and
+                (self.board[action] != 0))
+    
+    def get_utility(self):
+        return self.utility
+
+def minimax_decision(state):
+    if state.is_terminal_state():
+        return state.get_utility()
+
+    # store all the possible resulting utilities
+    results = []
+    for action in range(NUM_PITS):
+        if state.is_valid_action(action):
+            results.append(min_value(state.result(action)))
+        else:
+            results.append(-sys.maxint - 1)
+    
+    # maximize the result and return it
+    return results.index(max(results))
+
+if __name__ == '__main__':
+    init_board = ([NUM_STONES for i in range(NUM_PITS)] + [0]) * 2
+    init_state = state(init_board)
